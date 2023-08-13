d1bc1622036aaf2ec884bcf79d7608791c5d41de
fiaank123 <552137659@qq.com>
2023-08-13 19:14:06 +0800

    根据路由数据 (routes)生成菜单

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ import App from './App.vue'
 import 'virtual:svg-icons-register';    //引入SVG注册脚本
 import 'uno.css' //引入 uno.css
 import { createPinia } from "pinia"; //Pinia 是 Vue 的专属状态管理库，它允许你跨组件或页面共享状态
-createApp(App).use(createPinia()).mount('#app')
+// main.ts
+// 全局注册路由实例
+import router from "@/router";
+
+
+createApp(App).use(createPinia()).use(router).mount('#app')
--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -1,10 +1,81 @@
-// src/store/modules/permission.ts 
+//src/store/modules/permission.ts 
 // 获取当前登录用户的角色信息进行动态路由的初始化，最终调用 permissionStore.generateRoutes(roles) 方法生成动态路由
-import { listRoutes } from '@/api/menu';
 
-export const usePermissionStore = defineStore('permission', () => {
+import { RouteRecordRaw } from "vue-router";
+import { defineStore } from "pinia";
+import { constantRoutes } from "@/router";
+import { store } from "@/store";
+import { listRoutes } from "@/api/menu";
+
+const modules = import.meta.glob("../../views/**/**.vue");
+const Layout = () => import("@/layout/index.vue");
+
+/**
+ * Use meta.role to determine if the current user has permission
+ *
+ * @param roles 用户角色集合
+ * @param route 路由
+ * @returns
+ */
+const hasPermission = (roles: string[], route: RouteRecordRaw) => {
+  if (route.meta && route.meta.roles) {
+    // 角色【超级管理员】拥有所有权限，忽略校验
+    if (roles.includes("ROOT")) {
+      return true;
+    }
+    return roles.some((role) => {
+      if (route.meta?.roles !== undefined) {
+        return (route.meta.roles as string[]).includes(role);
+      }
+    });
+  }
+  return false;
+};
+
+/**
+ * 递归过滤有权限的异步(动态)路由
+ *
+ * @param routes 接口返回的异步(动态)路由
+ * @param roles 用户角色集合
+ * @returns 返回用户有权限的异步(动态)路由
+ */
+const filterAsyncRoutes = (routes: RouteRecordRaw[], roles: string[]) => {
+  const asyncRoutes: RouteRecordRaw[] = [];
+
+  routes.forEach((route) => {
+    const tmpRoute = { ...route }; // ES6扩展运算符复制新对象
+
+    // 判断用户(角色)是否有该路由的访问权限
+    if (hasPermission(roles, tmpRoute)) {
+      if (tmpRoute.component?.toString() == "Layout") {
+        tmpRoute.component = Layout;
+        console.log();
+      } else {
+        const component = modules[`../../views/${tmpRoute.component}.vue`];
+        if (component) {
+          tmpRoute.component = component;
+        } else {
+          tmpRoute.component = modules[`../../views/error-page/404.vue`];
+        }
+      }
+
+      if (tmpRoute.children) {
+        tmpRoute.children = filterAsyncRoutes(tmpRoute.children, roles);
+      }
+
+      asyncRoutes.push(tmpRoute);
+    }
+  });
+
+  return asyncRoutes;
+};
+
+// setup
+export const usePermissionStore = defineStore("permission", () => {
+  // state
   const routes = ref<RouteRecordRaw[]>([]);
 
+  // actions
   function setRoutes(newRoutes: RouteRecordRaw[]) {
     routes.value = constantRoutes.concat(newRoutes);
   }
@@ -24,12 +95,15 @@ export const usePermissionStore = defineStore('permission', () => {
           setRoutes(accessedRoutes);
           resolve(accessedRoutes);
         })
-        .catch(error => {
+        .catch((error) => {
           reject(error);
         });
     });
   }
-  // 导出 store 的动态路由数据 routes 
   return { routes, setRoutes, generateRoutes };
 });
 
+// 非setup
+export function usePermissionStoreHook() {
+  return usePermissionStore(store);
+}
