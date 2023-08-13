508fe5c45a7bc27a398b70c9c6f1291ad16b9693
fiaank123 <552137659@qq.com>
2023-08-13 19:15:13 +0800

    根据路由数据 (routes)生成菜单的关键代码

--- src/layout/componets/Sidebar/index.vue
+++ src/layout/componets/Sidebar/index.vue
@@ -4,16 +4,36 @@ const permissionStore = usePermissionStore();
 const appStore = useAppStore();
 const { sidebarLogo } = storeToRefs(settingsStore);
 const route = useRoute();
-
+import variables from "@/styles/variables.module.scss";
 </script>
 
 <template>
-  <div :class="{ 'has-logo': sidebarLogo}">
-  <logo v-if="sidebarLogo" :collapse="!appStore.sidebar.opened" />
+  <div :class="{ 'has-logo': sidebarLogo }">
+    <logo v-if="sidebarLogo" :collapse="!appStore.sidebar.opened" />
+    <el-scrollbar>
+      <el-menu
+        :default-active="route.path"
+        :collapse="!appStore.sidebar.opened"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+        :unique-opened="false"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item
+          v-for="route in permissionStore.routes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+          :is-collapse="!appStore.sidebar.opened"
+        />
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
 <style lang="scss" scoped>
-:deep(.setting-container) {
+:deep(.stting-container) {
   .setting-item {
     color: #fff;
     .svg-icon {
